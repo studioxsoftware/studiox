7e07335d1ed5142ca906ae463d3c019f0e9b5fcc
Long Huynh <long@vesta.net>
2017-06-14 08:47:12 -0500

    Remove unused methods of UnitOfWorkHelper

--- framework/src/StudioX/Domain/Uow/UnitOfWorkHelper.cs
+++ framework/src/StudioX/Domain/Uow/UnitOfWorkHelper.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Linq;
 using System.Reflection;
-using StudioX.Application.Services;
-using StudioX.Domain.Repositories;
 
 namespace StudioX.Domain.Uow
 {
@@ -12,36 +8,12 @@ namespace StudioX.Domain.Uow
     internal static class UnitOfWorkHelper
     {
         /// <summary>
-        /// Returns true if UOW must be used for given type as convention.
-        /// </summary>
-        /// <param name="type">Type to check</param>
-        public static bool IsConventionalUowClass(Type type)
-        {
-            return typeof(IRepository).IsAssignableFrom(type) || typeof(IApplicationService).IsAssignableFrom(type);
-        }
-
-        /// <summary>
         /// Returns true if given method has UnitOfWorkAttribute attribute.
         /// </summary>
         /// <param name="methodInfo">Method info to check</param>
         public static bool HasUnitOfWorkAttribute(MemberInfo methodInfo)
         {
             return methodInfo.IsDefined(typeof(UnitOfWorkAttribute), true);
-        }
-
-        /// <summary>
-        /// Returns UnitOfWorkAttribute it exists.
-        /// </summary>
-        /// <param name="methodInfo">Method info to check</param>
-        public static UnitOfWorkAttribute GetUnitOfWorkAttributeOrNull(MemberInfo methodInfo)
-        {
-            var attrs = methodInfo.GetCustomAttributes(typeof (UnitOfWorkAttribute), false).ToArray();
-            if (attrs.Length <= 0)
-            {
-                return null;
-            }
-
-            return (UnitOfWorkAttribute) attrs[0];
         }
     }
 }
