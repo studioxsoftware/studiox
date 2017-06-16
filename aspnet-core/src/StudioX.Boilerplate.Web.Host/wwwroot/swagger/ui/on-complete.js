b3255475b6a1d7bbf96c2dc6956e7cc9dcd23504
Long Huynh <huynhquanglong@gmail.com>
2017-06-16 09:17:18 -0500

    Add Bearer authorization for Swagger

---
+++ aspnet-core/src/StudioX.Boilerplate.Web.Host/wwwroot/swagger/ui/on-complete.js
@@ -0,0 +1,24 @@
+﻿var getCookieValue = function (key) {
+    var equalities = document.cookie.split('; ');
+    for (var i = 0; i < equalities.length; i++) {
+        if (!equalities[i]) {
+            continue;
+        }
+
+        var splitted = equalities[i].split('=');
+        if (splitted.length !== 2) {
+            continue;
+        }
+
+        if (decodeURIComponent(splitted[0]) === key) {
+            return decodeURIComponent(splitted[1] || '');
+        }
+    }
+
+    return null;
+};
+
+var csrfCookie = getCookieValue("StudioX.AuthToken");;
+var header = 'Bearer ' + csrfCookie;
+var csrfCookieAuth = new SwaggerClient.ApiKeyAuthorization("Authorization", header, "header");
+swaggerUi.api.clientAuthorizations.add("Authorization", csrfCookieAuth);
