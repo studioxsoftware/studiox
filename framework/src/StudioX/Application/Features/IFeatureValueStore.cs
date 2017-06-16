41b968f8a687f4792af69938a8e565325992dddf
Long Huynh <huynhquanglong@gmail.com>
2017-06-15 21:54:25 -0500

    Format feature code and xml document

--- framework/src/StudioX/Application/Features/IFeatureValueStore.cs
+++ framework/src/StudioX/Application/Features/IFeatureValueStore.cs
@@ -3,12 +3,12 @@ using System.Threading.Tasks;
 namespace StudioX.Application.Features
 {
     /// <summary>
-    /// Defines a store to get feature values.
+    ///     Defines a store to get feature values.
     /// </summary>
     public interface IFeatureValueStore
     {
         /// <summary>
-        /// Gets the feature value or null.
+        ///     Gets the feature value or null.
         /// </summary>
         /// <param name="tenantId">The tenant id.</param>
         /// <param name="feature">The feature.</param>
--- framework/src/StudioX/Application/Features/SimpleFeatureDependency.cs
+++ framework/src/StudioX/Application/Features/SimpleFeatureDependency.cs
@@ -3,25 +3,25 @@
 namespace StudioX.Application.Features
 {
     /// <summary>
-    /// Most simple implementation of <see cref="IFeatureDependency"/>.
-    /// It checks one or more features if they are enabled.
+    ///     Most simple implementation of <see cref="IFeatureDependency" />.
+    ///     It checks one or more features if they are enabled.
     /// </summary>
     public class SimpleFeatureDependency : IFeatureDependency
     {
         /// <summary>
-        /// A list of features to be checked if they are enabled.
+        ///     A list of features to be checked if they are enabled.
         /// </summary>
         public string[] Features { get; set; }
 
         /// <summary>
-        /// If this property is set to true, all of the <see cref="Features"/> must be enabled.
-        /// If it's false, at least one of the <see cref="Features"/> must be enabled.
-        /// Default: false.
+        ///     If this property is set to true, all of the <see cref="Features" /> must be enabled.
+        ///     If it's false, at least one of the <see cref="Features" /> must be enabled.
+        ///     Default: false.
         /// </summary>
         public bool RequiresAll { get; set; }
-        
+
         /// <summary>
-        /// Initializes a new instance of the <see cref="SimpleFeatureDependency"/> class.
+        ///     Initializes a new instance of the <see cref="SimpleFeatureDependency" /> class.
         /// </summary>
         /// <param name="features">The features.</param>
         public SimpleFeatureDependency(params string[] features)
@@ -30,11 +30,11 @@ namespace StudioX.Application.Features
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="SimpleFeatureDependency"/> class.
+        ///     Initializes a new instance of the <see cref="SimpleFeatureDependency" /> class.
         /// </summary>
         /// <param name="requiresAll">
-        /// If this is set to true, all of the <see cref="Features"/> must be enabled.
-        /// If it's false, at least one of the <see cref="Features"/> must be enabled.
+        ///     If this is set to true, all of the <see cref="Features" /> must be enabled.
+        ///     If it's false, at least one of the <see cref="Features" /> must be enabled.
         /// </param>
         /// <param name="features">The features.</param>
         public SimpleFeatureDependency(bool requiresAll, params string[] features)
@@ -43,7 +43,7 @@ namespace StudioX.Application.Features
             RequiresAll = requiresAll;
         }
 
-        /// <inheritdoc/>
+        /// <inheritdoc />
         public Task<bool> IsSatisfiedAsync(IFeatureDependencyContext context)
         {
             return context.TenantId.HasValue
