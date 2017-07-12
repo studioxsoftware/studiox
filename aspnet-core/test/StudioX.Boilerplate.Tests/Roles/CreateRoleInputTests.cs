4ff4c442d3125e29e738617e72e6759dc080a9bc
Long Huynh <huynhquanglong@gmail.com>
2017-07-12 13:41:18 -0500

    #38: [UnitTest] - RoleAppService test

---
+++ aspnet-core/test/StudioX.Boilerplate.Tests/Roles/CreateRoleInputTests.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Shouldly;
+using StudioX.Authorization.Roles;
+using StudioX.Boilerplate.Authorization;
+using StudioX.Boilerplate.Authorization.Roles;
+using StudioX.Boilerplate.Roles.Dto;
+using Xunit;
+
+namespace StudioX.Boilerplate.Tests.Roles
+{
+    public class CreateRoleInputTests : BoilerplateDtoTestBase<CreateRoleInput>
+    {
+        protected override CreateRoleInput GetDto()
+        {
+            return new CreateRoleInput
+            {
+                Name = "New Role",
+                DisplayName = "New Role",
+                Description = "New Role Description",
+                Permissions = new List<string>
+                    {
+                        PermissionNames.System.Administration.Users.MainMenu,
+                        PermissionNames.System.Administration.Roles.MainMenu
+                    }
+            };
+        }
+
+        [Fact]
+        public virtual async Task NameWithNullValueShouldCauseValidationFailure()
+        {
+            //Arrange
+            var input = GetDto();
+            input.Name = null;
+
+            //Act, Assert
+            await Validate(input)
+                .ShouldThrowAsync<ShouldAssertException>();
+        }
+
+
+        [Fact]
+        public virtual async Task NameWithEmptyValueShouldCauseValidationFailure()
+        {
+            //Arrange
+            var input = GetDto();
+            input.Name = string.Empty;
+
+            //Act, Assert
+            await Validate(input)
+                .ShouldThrowAsync<ShouldAssertException>();
+        }
+
+        [Fact]
+        public virtual async Task NameWithOverMaxLengthValueShouldCauseValidationFailure()
+        {
+            //Arrange
+            var input = GetDto();
+            input.Name = new string('a', StudioXRoleBase.MaxNameLength + 1);
+
+            //Act, Assert
+            await Validate(input)
+                .ShouldThrowAsync<ShouldAssertException>();
+        }
+
+        [Fact]
+        public virtual async Task NameWithSpacesInValueShouldBeValid()
+        {
+            //Arrange
+            var input = GetDto();
+            input.Name = "Role Name With Space";
+
+            //Act, Assert
+            await Validate(input);
+        }
+
+        [Fact]
+        public virtual async Task DisplayNameWithNullValueShouldCauseValidationFailure()
+        {
+            //Arrange
+            var input = GetDto();
+            input.DisplayName = null;
+
+            //Act, Assert
+            await Validate(input)
+                .ShouldThrowAsync<ShouldAssertException>();
+        }
+
+        [Fact]
+        public virtual async Task DisplayNameWithEmptyValueShouldCauseValidationFailure()
+        {
+            //Arrange
+            var input = GetDto();
+            input.DisplayName = string.Empty;
+
+            //Act, Assert
+            await Validate(input)
+                .ShouldThrowAsync<ShouldAssertException>();
+        }
+
+        [Fact]
+        public virtual async Task DisplayNameWithSpacesInValueShouldBeValid()
+        {
+            //Arrange
+            var input = GetDto();
+            input.DisplayName = "Role DisplayName With Space";
+
+            //Act, Assert
+            await Validate(input);
+        }
+
+        [Fact]
+        public virtual async Task DisplayNameWithOverMaxLengthValueShouldCauseValidationFailure()
+        {
+            //Arrange
+            var input = GetDto();
+            input.DisplayName = new string('a', StudioXRoleBase.MaxDisplayNameLength + 1);
+
+            //Act, Assert
+            await Validate(input)
+                .ShouldThrowAsync<ShouldAssertException>();
+        }
+
+        [Fact]
+        public virtual async Task DescriptionWithNullValueShouldBeValid()
+        {
+            //Arrange
+            var input = GetDto();
+            input.Description = null;
+
+            //Act, Assert
+            await Validate(input);
+        }
+
+        [Fact]
+        public virtual async Task DescriptionWithEmptyValueShouldBeValid()
+        {
+            //Arrange
+            var input = GetDto();
+            input.Description = string.Empty;
+
+            //Act, Assert
+            await Validate(input);
+        }
+
+        [Fact]
+        public virtual async Task DescriptionWithOverMaxLengthValueShouldCauseValidationFailure()
+        {
+            //Arrange
+            var input = GetDto();
+            input.Description = new string('a', Role.MaxDescriptionLength + 1);
+
+            //Act, Assert
+            await Validate(input)
+                .ShouldThrowAsync<ShouldAssertException>();
+        }
+    }
+}
---
+++ aspnet-core/test/StudioX.Boilerplate.Tests/Roles/UpdateRoleInputTests.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Shouldly;
+using StudioX.Authorization.Roles;
+using StudioX.Boilerplate.Authorization.Roles;
+using StudioX.Boilerplate.Roles.Dto;
+using Xunit;
+
+namespace StudioX.Boilerplate.Tests.Roles
+{
+    public class UpdateRoleInputTests : BoilerplateDtoTestBase<UpdateRoleInput>
+    {
+        protected override UpdateRoleInput GetDto()
+        {
+            return new UpdateRoleInput
+            {
+                Id = 1,
+                Name = "Role",
+                DisplayName = "Role",
+                Description = "Role Description",
+                Permissions = new List<string> {"PermissionName"}
+            };
+        }
+
+        [Fact]
+        public virtual async Task NameWithNullValueShouldCauseValidationFailure()
+        {
+            //Arrange
+            var updateInput = GetDto();
+            updateInput.Name = null;
+
+            //Act, Assert
+            await Validate(updateInput).ShouldThrowAsync<ShouldAssertException>();
+        }
+
+        [Fact]
+        public virtual async Task NameWithEmptyValueShouldCauseValidationFailure()
+        {
+            //Arrange
+            var updateInput = GetDto();
+            updateInput.Name = string.Empty;
+
+            //Act, Assert
+            await Validate(updateInput).ShouldThrowAsync<ShouldAssertException>();
+        }
+
+        [Fact]
+        public virtual async Task NameWithOverMaxLengthValueShouldCauseValidationFailure()
+        {
+            //Arrange
+            var updateInput = GetDto();
+            updateInput.Name = new string('a', StudioXRoleBase.MaxNameLength + 1);
+
+            //Act, Assert
+            await Validate(updateInput).ShouldThrowAsync<ShouldAssertException>();
+        }
+
+        [Fact]
+        public virtual async Task NameWithSpacesInValueShouldBeValid()
+        {
+            //Arrange
+            var updateInput = GetDto();
+            updateInput.Name = "Role Name With Space";
+
+            //Act, Assert
+            await Validate(updateInput);
+        }
+
+        [Fact]
+        public virtual async Task DisplayNameWithNullValueShouldCauseValidationFailure()
+        {
+            //Arrange
+            var updateInput = GetDto();
+            updateInput.DisplayName = null;
+
+            //Act, Assert
+            await Validate(updateInput).ShouldThrowAsync<ShouldAssertException>();
+        }
+
+        [Fact]
+        public virtual async Task DisplayNameWithEmptyValueShouldCauseValidationFailure()
+        {
+            //Arrange
+            var updateInput = GetDto();
+            updateInput.DisplayName = string.Empty;
+
+            //Act, Assert
+            await Validate(updateInput).ShouldThrowAsync<ShouldAssertException>();
+        }
+
+        [Fact]
+        public virtual async Task DisplayNameWithSpacesInValueShouldBeValid()
+        {
+            //Arrange
+            var updateInput = GetDto();
+            updateInput.DisplayName = "Role DisplayName With Space";
+
+            //Act, Assert
+            await Validate(updateInput);
+        }
+
+        [Fact]
+        public virtual async Task DisplayNameWithOverMaxLengthValueShouldCauseValidationFailure()
+        {
+            //Arrange
+            var updateInput = GetDto();
+            updateInput.DisplayName = new string('a', StudioXRoleBase.MaxDisplayNameLength + 1);
+
+            //Act, Assert
+            await Validate(updateInput).ShouldThrowAsync<ShouldAssertException>();
+        }
+
+        [Fact]
+        public virtual async Task DescriptionWithNullValueShouldBeValid()
+        {
+            //Arrange
+            var updateInput = GetDto();
+            updateInput.Description = null;
+
+            //Act, Assert
+            await Validate(updateInput);
+        }
+
+        [Fact]
+        public virtual async Task DescriptionWithEmptyValueShouldBeValid()
+        {
+            //Arrange
+            var updateInput = GetDto();
+            updateInput.Description = string.Empty;
+
+            //Act, Assert
+            await Validate(updateInput);
+        }
+
+        [Fact]
+        public virtual async Task DescriptionWithOverMaxLengthValueShouldCauseValidationFailure()
+        {
+            //Arrange
+            var updateInput = GetDto();
+            updateInput.Description = new string('a', Role.MaxDescriptionLength + 1);
+
+            //Act, Assert
+            await Validate(updateInput).ShouldThrowAsync<ShouldAssertException>();
+        }
+    }
+}
